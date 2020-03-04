0338b8035dbab435445d10cec57328d81f052fb1
GhadeerQalas <ghadeer.balghaith18@gmail.com>
2020-03-04 22:44:42 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-   // return (
-   //   <div className="App">
-   //     <h1>I'm a react developer</h1>
-    //  </div>
-   // );
-   return React.createElement('div',{className: 'App'}, React.createElement('h1',null,'Does this work now?'));
+    return (
+      <div className="App">
+       <h1>I'm a react developer</h1>
+       <p>This is really working! </p>
+       <Person name="Ghadeer" age="23">My hobbies: Racing </Person>
+       <Person name="Afnan" age="27"/>
+       <Person name="Zainab" age="47"/>
+      </div>
+    );
+   // return React.createElement('div',{className: 'App'}, React.createElement('h1',null,'Does this work now?'));
   }
 }
 
