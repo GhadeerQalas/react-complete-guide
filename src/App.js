9ebbea37f38b7aa4f24040f1a57d72f847a2255d
GhadeerQalas <ghadeer.balghaith18@gmail.com>
2020-03-07 12:40:13 +0300

    new state code

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+ state = {
+   persons: [
+     {name: 'Max', age:28},
+     {name: 'Manu', age:27},
+     {name: 'Steph', age:26}
+   ]
+ }
+
+
   render() {
     return (
       <div className="App">
        <h1>I'm a react developer</h1>
        <p>This is really working! </p>
-       <Person name="Ghadeer" age="23">My hobbies: Racing </Person>
-       <Person name="Afnan" age="27"/>
+       <button>Switch Name</button>
+       <Person name="Ghadeer" age="23"/>
+       <Person name="Afnan" age="27">  My hobbies: Racing </Person>
        <Person name="Zainab" age="47"/>
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,10 +5,10 @@ const person = (props) => {
     return (
     <div>
         <p>I'm {props.name} and I am {props.age} years old</p>
-        <p>{props.childern}</p>
+        <p> {props.childern} </p>
     </div>
     
-)
+   )
 };
 
 
