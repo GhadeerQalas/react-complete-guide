a1d0c3225397076c5ffe478d13ba61f9c0701710
GhadeerQalas <ghadeer.balghaith18@gmail.com>
2020-03-07 13:54:10 +0300

    react hook

--- src/App.js
+++ src/App.js
@@ -2,25 +2,39 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
+
+// this react hooks 
+const app = props => {
  state = {
    persons: [
      {name: 'Max', age:28},
      {name: 'Manu', age:27},
      {name: 'Steph', age:26}
-   ]
+   ],
+   otherState: 'some other value'
  }
 
+ switchNameHandler = () =>{
+   // Console.log('Was clicked!');
+   // Don't DO THIS: this.state.persons[0].name = "Maximilian";
+   this.setState({persons: [
+        {name: 'Maximilian', age:28},
+        {name: 'Manu', age:27},
+        {name: 'Steph', age:25}
+       ] // end array of persons 
+        } // end pranthses of setstate
+        );
+ }
 
   render() {
     return (
       <div className="App">
        <h1>I'm a react developer</h1>
        <p>This is really working! </p>
-       <button>Switch Name</button>
-       <Person name="Ghadeer" age="23"/>
-       <Person name="Afnan" age="27">  My hobbies: Racing </Person>
-       <Person name="Zainab" age="47"/>
+       <button onClick={this.switchNameHandler}>Switch Name</button>
+       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>  My hobbies: Racing </Person>
+       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
    // return React.createElement('div',{className: 'App'}, React.createElement('h1',null,'Does this work now?'));
