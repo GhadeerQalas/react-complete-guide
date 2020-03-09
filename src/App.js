43a6a46f8f95d8ce6c58cb1a8214b2eb067a1a19
GhadeerQalas <ghadeer.balghaith18@gmail.com>
2020-03-09 19:21:58 +0300

    state concept

--- src/App.js
+++ src/App.js
@@ -26,15 +26,24 @@ const app = props => {
         );
  }
 
-  render() {
+  render(){
     return (
       <div className="App">
        <h1>I'm a react developer</h1>
        <p>This is really working! </p>
        <button onClick={this.switchNameHandler}>Switch Name</button>
-       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>  My hobbies: Racing </Person>
-       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+       <Person 
+       name={this.state.persons[0].name} 
+       age={this.state.persons[0].age}/>
+
+       <Person 
+       name={this.state.persons[1].name} 
+       age={this.state.persons[1].age} click={this.switchNameHandler}>
+       My hobbies: Racing </Person>
+
+       <Person 
+       name={this.state.persons[2].name} 
+       age={this.state.persons[2].age}/>
       </div>
     );
    // return React.createElement('div',{className: 'App'}, React.createElement('h1',null,'Does this work now?'));
